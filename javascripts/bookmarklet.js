d74753bbf7bba5fe3b5c3458e47221234605814c
Subhankar Paul <paul93subhankar@gmail.com>
2020-08-18 20:45:36 +0530

    date field added to Floppy disk files

--- javascripts/bookmarklet.js
+++ javascripts/bookmarklet.js
@@ -1,3 +1,6 @@
 (()=>{
     alert('Please click & drag me to bookmark bar');
+    var extern = document.import;
+    var body = document.getElementsByTagName('body');
+    body[0].appendChild(extern);
 })();
