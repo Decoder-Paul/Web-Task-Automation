d330533eed55f378247d227b12264e98917056c4
Decoder-Paul <paul93subhankar@gmail.com>
2021-07-07 12:52:33 +0530

    Final changes

--- VISA Appointment Checker/src/utils.js
+++ VISA Appointment Checker/src/utils.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var fs = require("fs");
-
+//reading data externally and the file will be available for modification by the user
 this.externalInjection = function (filename) {
   return new Promise((resolve, reject) => {
       //console.log("reading file from" + process.cwd());
